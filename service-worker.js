05d381d90dbbe348c24904824f68a66746d50e6d
DitRadit <adiradityagt@gmail.com>
2025-12-09 09:38:39 +0700

    update

--- service-worker.js
+++ service-worker.js
@@ -4,3 +4,11 @@ workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
     new workbox.strategies.NetworkFirst()
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(reg => console.log('SW registered:', reg))
+      .catch(err => console.log('SW registration failed:', err));
+  });
+}
