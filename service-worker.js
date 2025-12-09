3ae94c8edf536beed674b76b5af137dd47ba09e5
DitRadit <adiradityagt@gmail.com>
2025-12-09 09:44:44 +0700

    update

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,16 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
+// Cache images
 workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
-    new workbox.strategies.NetworkFirst()
+  ({request}) => request.destination === 'image',
+  new workbox.strategies.NetworkFirst()
 );
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js')
-      .then(reg => console.log('SW registered:', reg))
-      .catch(err => console.log('SW registration failed:', err));
-  });
-}
+// Cache HTML, CSS, JS
+workbox.routing.registerRoute(
+  ({request}) => 
+    request.destination === 'document' ||
+    request.destination === 'script' ||
+    request.destination === 'style',
+  new workbox.strategies.StaleWhileRevalidate()
+);
